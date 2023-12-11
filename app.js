535ea6e4380bbd30191df3de757ea3a246ed570c
kadir-zeren <k4dzeren@gmail.com>
2023-12-11 22:06:13 +0300

    js comment

---
+++ app.js
@@ -0,0 +1,11 @@
+//* Javascript Kodu */
+// console.log("Localde bulunan script calisti")
+
+// alert("Bu bir alert dir") /* Alert uygulmayı bloklar (Blocking Code)*/
+
+// karar = confirm("Emin msiniz E/H")
+// console.log(karar)
+
+// console.warn("Bu bir uyarıdir")
+
+// console.error("Eyvah hata olustu..")
